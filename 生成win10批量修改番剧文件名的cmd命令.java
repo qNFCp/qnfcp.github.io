20756b327289de8dc6f5da52b151cf7c3e44c92d
qNFCp <i@qnfcp.xyz>
2021-02-25 04:14:17 +0800

    更改为文件名全文匹配

--- 生成win10批量修改番剧文件名的cmd命令.java
+++ 生成win10批量修改番剧文件名的cmd命令.java
@@ -1,14 +1,47 @@
-	public static void main(String[] args) {
+public static void main(String[] args) {
+		//ren 原文件名 新文件名
 		System.out.println("chcp 65001\r\n" + "pause"); //cmd编码切换为UTF-8 防乱码
-		for(int i=58;i<=144;i++) { //控制集数
-			//此处的格式为 *数字*.mp4 修改
-			System.out.print("ren *"); 
+		int temp=0;
+		for(int i=1;i<=26;i++) { //控制集数
+			//原文件名
+			System.out.print("ren \"魔法科高校的劣等生S1 E"); //精准文件名
+			//System.out.print("ren *");  //非精准
 			if(i<10)
 				System.out.print(0);
-			System.out.print(i + "*.mp4 "+"\"番名 E");
+			System.out.print(i + ".mp4\"");//文件格式
+			//System.out.print(i + "*.mp4 "+"\"番名 E");//非精准
+			
+			//新文件名
+			System.out.print(" \"S01 E");//精准文件名前缀
 			if(i<10)
 				System.out.print(0);
-			System.out.println(i+".mp4\"");
+			System.out.println(i+".mp4\"");//精准文件名后戳
 		}
+		/*
+		for(int i=1;i<=25;i++) { 
+			System.out.print("ren \"S03 E"); 
+			if(i<10)
+				System.out.print(0);
+			System.out.print(i + ".sc.ass\"");
+			
+			System.out.print(" \"S01 E");
+			if(i<10)
+				System.out.print(0);
+			System.out.println(i+".sc.ass\"");
+		}
+		
+		for(int i=1;i<=25;i++) { 
+			System.out.print("ren \"S03 E"); 
+			if(i<10)
+				System.out.print(0);
+			System.out.print(i + ".tc.ass\"");
+			
+			System.out.print(" \"S01 E");
+			if(i<10)
+				System.out.print(0);
+			System.out.println(i+".tc.ass\"");
+		}
+		*/
+		
 		System.out.print("pause");
 	}
