05c92aacfa391ca5d94dfed2a2f32f8663db5552
qNFCp <i@qnfcp.xyz>
2021-02-25 18:52:31 +0800

    Update miganBT.cpp
    Optimised the generated file names. Currently only runs on the desktop. 优化了生成的文件名。目前只能在桌面运行

--- miganBT.cpp
+++ miganBT.cpp
@@ -6,30 +6,54 @@ int main(int argc, char* argv[]){
 	//ios::sync_with_stdio(false);
 	for(int i=0;i<40;i++) cout<<" "; 
 	cout<<"蜜柑计划 种子批量导出器\n\n";
+	
+	//下载rss 
 	char migan[3253]={0},key[610]={0};
-	char ed[]={"\',\'\%userprofile\%\\desktop\\a.xml\')"};
-	//char ed[]={"\',\'E:\\OneDrive\\桌面\\a.xml\')"};
+	//char deskpath[255]=	{"E:\\OneDrive\\桌面\\"};
+	char deskpath[255]={"\%userprofile\%\\desktop\\"};
 	cout<<"键入关键词:";
 	cin>>key;
-	sprintf(migan,"powershell (new-object System.Net.WebClient).DownloadFile( \'https://mikanani.me/RSS/Search?searchstr=%s%s",key,ed);//set migan, migan+=key		
+	sprintf(migan,"powershell (new-object System.Net.WebClient).DownloadFile( \'https://mikanani.me/RSS/Search?searchstr=%s\',\'%stemp.xml\')",key,deskpath);//set migan, migan+=key		
 	system(migan);	
-	freopen("a.xml", "r", stdin);
-	freopen("a.txt", "w", stdout);
+	
+	//读入及写入连接 
+	freopen("temp.xml", "r", stdin);
+	freopen("temp.txt", "w", stdout);
+	
+	//内容检索 
 	string link;
+	int times=0;
 	while(cin>>link){
 		if(link.rfind("url=",0)==0){ //搜索内容 "url="
 			link.erase(0,5);//删除前5位字符 
 			replace(link.begin(),link.end(),'\"','\0');	//替换"号 
 			cout<<link<<'\n';
+			times++;
 		}
-		if(link.rfind("</rss>",0)==0)
-		break;
+		if(link.rfind("</rss>",0)==0)//判断结束读入 
+			break;
 	}	
+	
+	//断开文件连接 
 	fclose(stdin);
 	fclose(stdout);
-	char del[255]={0},deskpath[255]={"\"\%userprofile\%\\desktop\\a.xml\""};
-	//char del[255]={0},deskpath[255]={"\"E:\\OneDrive\\桌面\\a.xml\""};	
-	sprintf(del,"del %s",deskpath);//set migan, migan+=key		
-	system(del);	
+	
+	//删除xml文件 
+	char del[255]={0};
+	sprintf(del,"del \"%stemp.xml\"",deskpath);	
+	system(del);
+	
+	//搜索结果为空时 删除空文件 
+	if(times==0){
+		sprintf(del,"del \"%stemp.txt\"",deskpath);	
+		system(del);
+		return 0;
+	}
+	
+	//修改文件名
+	char rename[255]={0};
+	sprintf(rename,"ren \"%stemp.txt\" %s.txt\"",deskpath,key);
+	cout<<rename; 
+	system(rename);	
 	return 0;
 } 
