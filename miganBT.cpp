b7379d249d48c80a90e4617b6df6d869f95d2577
qNFCp <i@qnfcp.xyz>
2021-02-25 16:12:35 +0800

    Update miganBT.cpp
    Added section for direct download analysis 增加了直接下载并分析的功能

--- miganBT.cpp
+++ miganBT.cpp
@@ -1,18 +1,18 @@
-#include <bits/stdc++.h>
+#include <bits/stdc++.h> 
+#include <windows.h>
 using namespace std;
 
-int main(){
+int main(int argc, char* argv[]){
 	//ios::sync_with_stdio(false);
 	for(int i=0;i<40;i++) cout<<" "; 
 	cout<<"蜜柑计划 种子批量导出器\n\n";
 	char migan[3253]={0},key[610]={0};
+	char ed[]={"\',\'\%userprofile\%\\desktop\\a.xml\')"};
+	//char ed[]={"\',\'E:\\OneDrive\\桌面\\a.xml\')"};
 	cout<<"键入关键词:";
 	cin>>key;
-	cout<<"\n请将弹出页面右键另存为a.xml,并置于本程序相同目录下\n"; 
-	sprintf(migan,"start https://mikanani.me/RSS/Search?searchstr=%s",key);//set migan, migan+=key
-	system(migan);
-	cout<<"\n输出文件名为a.txt\n";
-	system("pause");
+	sprintf(migan,"powershell (new-object System.Net.WebClient).DownloadFile( \'https://mikanani.me/RSS/Search?searchstr=%s%s",key,ed);//set migan, migan+=key		
+	system(migan);	
 	freopen("a.xml", "r", stdin);
 	freopen("a.txt", "w", stdout);
 	string link;
@@ -22,6 +22,14 @@ int main(){
 			replace(link.begin(),link.end(),'\"','\0');	//替换"号 
 			cout<<link<<'\n';
 		}
-	}
+		if(link.rfind("</rss>",0)==0)
+		break;
+	}	
+	fclose(stdin);
+	fclose(stdout);
+	char del[255]={0},deskpath[255]={"\"\%userprofile\%\\desktop\\a.xml\""};
+	//char del[255]={0},deskpath[255]={"\"E:\\OneDrive\\桌面\\a.xml\""};	
+	sprintf(del,"del %s",deskpath);//set migan, migan+=key		
+	system(del);	
 	return 0;
 } 
